c7a1db879ff050c4fc6bed9d4ca04514f47fdb14
EvgeniyGrekov <evgeniygrekov@mail.ru>
2025-09-09 20:37:08 +0300

    Сделал Д3#2

---
+++ src/app/components/to-do-list-item/to-do-list-item.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from '../to-do-list/to-do-list';
+
+@Component({
+  selector: 'app-to-do-list-item',
+  imports: [],
+  templateUrl: './to-do-list-item.html',
+  styleUrl: './to-do-list-item.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ToDoListItem {
+  @Input({required: true}) public task!: Task
+  @Output() protected readonly doDeleteTask: EventEmitter<number> = new EventEmitter<number>
+}
--- src/app/components/to-do-list/to-do-list.ts
+++ src/app/components/to-do-list/to-do-list.ts
@@ -1,23 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ToDoListItem } from "../to-do-list-item/to-do-list-item";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
-
-interface Task {
-  id: number,
-  todoTitle: string
+export class Task {
+  constructor(  
+    public id: number,
+    public todoTitle: string
+  ) {}
 }
 
 const tasks: Task[] = [
-    {id: 0, todoTitle: "Написать мотивационный пост"},
-    {id: 1, todoTitle: "Выполнить ДЗ#1"},
-    {id: 2, todoTitle: "Послушать онлайн-урок"}
-  ]
+    new Task(0, "Написать мотивационный пост"),
+    new Task(1, "Выполнить ДЗ#1"),
+    new Task(2, "Послушать онлайн-урок")
+]
 
 @Component({
   selector: 'app-to-do-list',
-  imports: [],
+  imports: [FormsModule, CommonModule, ToDoListItem, MatFormFieldModule, MatInputModule],
   templateUrl: './to-do-list.html',
-  styleUrl: './to-do-list.css'
+  styleUrl: './to-do-list.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ToDoList {
-  protected readonly tasks: Task[] = tasks
+
+  protected tasks: Task[] = tasks
+  protected newTaskText: string = ""
+
+  protected isDisabledAddButton(): boolean {
+    return !this.newTaskText || this.newTaskText.trim().length === 0
+  }
+
+  protected doDeleteTask(id: number): void {
+    this.tasks.forEach((task, index) => {
+      if(task.id === id) {
+        this.tasks.splice(index, 1)
+      }
+    })
+  }
+
+protected doAddNewTask(): void {
+  let idMax = Math.max(0, ...this.tasks.map(task => task.id))
+  this.tasks.push(new Task(++idMax, this.newTaskText))
+}
+
 }
